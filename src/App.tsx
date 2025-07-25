140cc5e4e8ec24ea3ec5e66f727f4f6f5aa97bb5
rawbin <fcrshjx44@gmail.com>
2025-07-25 10:34:57 +0800

    feat: 增加 framer 滚动视差效果

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { motion, MotionConfig } from 'motion/react'
+
+const initial = {
+    opacity: 0, /* 初始透明度为0 */
+    transform: 'translateY(50px)' /* 初始位置从下方50px开始 */
+}
+
+const whileInView = {
+    opacity: 1, /* 最终透明度为1 */
+    transform: 'translateY(0)' /* 回到正常位置 */
+}
+
+const motionProps = {
+    layout:true,
+    initial,
+    whileInView,
+    viewPort:{
+        once: true  //去掉这个便于调试
+    }
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,39 +29,43 @@ function App() {
   const getBlocks = () => {
       return Array(8).fill('').map((item,index) => {
           return <div>
-              <h1>{
+              <motion.h1 {...motionProps}>{
                   `${index}`.repeat(20)
-              }</h1>
-              <div>
+              }</motion.h1>
+              <motion.div {...motionProps}>
                   <a href="https://vite.dev" target="_blank">
                       <img src={viteLogo} className="logo" alt="Vite logo" />
                   </a>
                   <a href="https://react.dev" target="_blank">
                       <img src={reactLogo} className="logo react" alt="React logo" />
                   </a>
-              </div>
-              <h1>Vite + React</h1>
-              <div className="card">
+              </motion.div>
+              <motion.h1 {...motionProps}>Vite + React</motion.h1>
+              <motion.div {...motionProps} className="card">
                   <button onClick={() => setCount((count) => count + 1)}>
                       count is {count}
                   </button>
                   <p>
                       Edit <code>src/App.tsx</code> and save to test HMR
                   </p>
-              </div>
-              <p className="read-the-docs">
+              </motion.div>
+              <motion.p {...motionProps} className="read-the-docs">
                   Click on the Vite and React logos to learn more
-              </p>
+              </motion.p>
           </div>
       })
   }
 
   return (
-          <>
+          <MotionConfig transition={{
+            duration: 1,
+              delay:1,
+            ease: 'easeOut'
+          }}>
               {
                   getBlocks()
               }
-              </>
+              </MotionConfig>
   )
 }
 
